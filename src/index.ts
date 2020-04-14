e63fee6e79034fb8ff295e598338cc1f185f70ad
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-14 16:16:26 +0800

    feat: Add bootstrap code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 import 'module-alias/register';
+import express from 'express';
+
+async function bootstrap() {
+  const port = Number(process.env.PORT) || 9000;
+  const app = express();
+  app.listen(port, '0.0.0.0', () =>
+    console.log(`🚀 Server running on http://0.0.0.0:${port}`),
+  );
+
+  app.get('/', (req) => {
+    console.log(req.url);
+  });
+}
+
+bootstrap();
