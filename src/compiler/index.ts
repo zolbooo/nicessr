ff880d40b3df633a0112571062e5af15c23d06a3
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-16 15:58:10 +0800

    feat: Add SSR compiler

---
+++ src/compiler/index.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import webpack from 'webpack';
+
+import InjectPlugin, { ENTRY_ORDER } from 'webpack-inject-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+import webpackModules from './modules';
+
+export const ssrCompiler = webpack({
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: [],
+  watch: true,
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(process.cwd(), '.nicessr', 'ssr'),
+    filename: '[chunkhash].js',
+    libraryTarget: 'window',
+  },
+  module: webpackModules,
+  optimization: {
+    splitChunks: { chunks: 'all' },
+    runtimeChunk: 'single',
+  },
+  resolve: {
+    alias: {
+      nicessr: path.join(__dirname, '..', 'csr'),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new InjectPlugin(() => `require('nicessr/runtime').clientEntrypoint()`, {
+      entryOrder: ENTRY_ORDER.First,
+    }),
+  ],
+});
