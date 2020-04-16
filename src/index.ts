d84cc37f2c2053efc72dab2d244bbd30f20235be
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-16 17:39:10 +0800

    fix: Use Bundler for builds and updates

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
 import path from 'path';
 import express from 'express';
 
-import { resolveURL } from './util';
 import { renderPage } from './ssr/renderer';
-import { compiledPages } from './ssr/compiler';
-import { unsubscribe, subscribeForPageUpdates } from './ssr/auto-reload';
+import { Bundler, BuildEvent } from './compiler/bundler';
 
 async function bootstrap() {
   const port = Number(process.env.PORT) || 9000;
   const app = express();
   const server = app.listen(port, '0.0.0.0', () =>
-    console.log(`🚀 Server running on http://0.0.0.0:${port}`),
+    console.log(`🚀\tServer running on http://0.0.0.0:${port}`),
   );
 
+  const bundler = new Bundler();
   app.use('/.nicessr/auto-refresh', (req, res) => {
-    const page = resolveURL(req.query.page.toString());
-    if (!compiledPages.get(page)) {
-      res.status(404).send('Page not found');
-      return;
-    }
+    const handleBuildEvent = (event: BuildEvent) => {
+      if (event.status === 'success') {
+        res.write(`id: ${event.bundle.ssr[0]}\n`);
+        res.write('data: {"type": "update"}\n\n');
+      }
+    };
 
+    bundler.subscribe(req.path, handleBuildEvent);
     res.socket.setTimeout(1000 * 60 * 60 * 24);
     res.writeHead(200, {
       'Content-Type': 'text/event-stream',
@@ -27,26 +28,27 @@ async function bootstrap() {
       Connection: 'keep-alive',
     });
     res.write('\n');
-
-    subscribeForPageUpdates(page, res);
-    req.on('close', () => unsubscribe(page, res));
+    req.on('close', () => bundler.unsubscribe(req.path, handleBuildEvent));
   });
+
   app.use(
     '/.nicessr',
     express.static(path.join(process.cwd(), '.nicessr', 'ssr')),
   );
   app.get('*', async (req, res, next) => {
-    const markup = await renderPage(req.path, { req, res });
-    if (markup === null) {
-      next();
-      return;
+    const buildResult = await bundler.buildOnce(req.path);
+    if (buildResult.status !== 'success') {
+      return next();
     }
+
+    const markup = await renderPage(req.path, { req, res }, buildResult.bundle);
     res.status(200).send(markup);
   });
+
   app.use(express.static(path.join(process.cwd(), 'public')));
 
   process.on('SIGINT', () => {
-    console.log('\n👾 Exiting gracefully, please wait...');
+    console.log('\n👾\tExiting gracefully, please wait...');
     server.close();
     process.exit(0);
   });
