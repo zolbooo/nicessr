8d7fcdcf051d4ed98d7a8256d4e462952f543c6a
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-16 23:35:38 +0800

    feat: Add terser plugin for production

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -2,10 +2,13 @@ import path from 'path';
 import webpack from 'webpack';
 import nodeExternals from 'webpack-node-externals';
 
+import TerserPlugin from 'terser-webpack-plugin';
 import InjectPlugin, { ENTRY_ORDER } from 'webpack-inject-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 import webpackModules from './modules';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const buildPathSSR = path.join(process.cwd(), '.nicessr', 'ssr');
 export const buildPathClient = path.join(process.cwd(), '.nicessr', 'build');
@@ -15,11 +18,10 @@ export const createCompiler = (
 ) =>
   webpack([
     {
-      mode:
-        process.env.NODE_ENV === 'production' ? 'production' : 'development',
+      mode: isProduction ? 'production' : 'development',
       entry: getEntrypoints('ssr:'),
       watch: true,
-      devtool: 'inline-source-map',
+      devtool: isProduction ? false : 'inline-source-map',
       output: {
         path: buildPathSSR,
         filename: '[chunkhash].js',
@@ -28,14 +30,23 @@ export const createCompiler = (
       module: webpackModules,
       target: 'node',
       externals: [nodeExternals()],
+      resolve: {
+        alias: {
+          nicessr: path.join(__dirname, '..', '..'),
+        },
+      },
+      optimization: {
+        usedExports: true,
+        minimize: isProduction,
+        minimizer: [new TerserPlugin()],
+      },
       plugins: [new CleanWebpackPlugin()],
     },
     {
-      mode:
-        process.env.NODE_ENV === 'production' ? 'production' : 'development',
+      mode: isProduction ? 'production' : 'development',
       entry: getEntrypoints('client:'),
       watch: true,
-      devtool: 'source-map',
+      devtool: isProduction ? false : 'source-map',
       output: {
         path: buildPathClient,
         filename: '[chunkhash].js',
@@ -45,11 +56,19 @@ export const createCompiler = (
       optimization: {
         splitChunks: { chunks: 'all' },
         runtimeChunk: 'single',
+        usedExports: true,
+        minimize: isProduction,
+        minimizer: [new TerserPlugin()],
+      },
+      resolve: {
+        alias: {
+          nicessr: path.join(__dirname, '..', '..'),
+        },
       },
       plugins: [
         new CleanWebpackPlugin(),
         new InjectPlugin(
-          () => `require('nicessr/runtime').clientEntrypoint()`,
+          () => `require('nicessr/dist/csr/runtime').clientEntrypoint()`,
           {
             entryOrder: ENTRY_ORDER.First,
           },
