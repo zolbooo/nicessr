88a2bcd070e741598a81740a3dddaa514dc93862
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-16 17:33:43 +0800

    refactor: Use Bundle instance for SSR

--- src/util.ts
+++
@@ -1,7 +0,0 @@
-import { compiledPages } from './ssr/compiler';
-
-export function resolveURL(url: string) {
-  if (url.endsWith('/')) return url + 'index';
-  if (compiledPages.has(url)) return url;
-  return url + '/index';
-}
