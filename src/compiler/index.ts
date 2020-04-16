4f69ce68f79c1df65246fd222604d8decc212477
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-16 17:12:47 +0800

    refactor: Make webpack compiler dynamic

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -6,30 +6,33 @@ import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 import webpackModules from './modules';
 
-export const ssrCompiler = webpack({
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: [],
-  watch: true,
-  devtool: 'inline-source-map',
-  output: {
-    path: path.join(process.cwd(), '.nicessr', 'ssr'),
-    filename: '[chunkhash].js',
-    libraryTarget: 'window',
-  },
-  module: webpackModules,
-  optimization: {
-    splitChunks: { chunks: 'all' },
-    runtimeChunk: 'single',
-  },
-  resolve: {
-    alias: {
-      nicessr: path.join(__dirname, '..', 'csr'),
+export const createCompilerSSR = (
+  getEntrypoints: () => { [key: string]: string },
+) =>
+  webpack({
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: getEntrypoints,
+    watch: true,
+    devtool: 'inline-source-map',
+    output: {
+      path: path.join(process.cwd(), '.nicessr', 'ssr'),
+      filename: '[chunkhash].js',
+      libraryTarget: 'window',
     },
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new InjectPlugin(() => `require('nicessr/runtime').clientEntrypoint()`, {
-      entryOrder: ENTRY_ORDER.First,
-    }),
-  ],
-});
+    module: webpackModules,
+    optimization: {
+      splitChunks: { chunks: 'all' },
+      runtimeChunk: 'single',
+    },
+    resolve: {
+      alias: {
+        nicessr: path.join(__dirname, '..', 'csr'),
+      },
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new InjectPlugin(() => `require('nicessr/runtime').clientEntrypoint()`, {
+        entryOrder: ENTRY_ORDER.First,
+      }),
+    ],
+  });
