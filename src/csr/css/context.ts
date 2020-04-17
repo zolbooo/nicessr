804580d4dc31a7b7422f2cb75cbec6eef2855357
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-17 10:39:43 +0800

    fix: Replace all digits in hash with letters

--- src/csr/css/context.ts
+++ src/csr/css/context.ts
@@ -3,7 +3,11 @@ import sha256 from 'sha256';
 const styles = new Map<string, string>();
 
 export function assignClass(css: string): string {
-  const hash = sha256(css);
+  const hash: string = sha256(css).replace(/[0-9]/g, (digit) =>
+    String.fromCharCode(
+      digit.charCodeAt(0) - '0'.charCodeAt(0) + 'a'.charCodeAt(0),
+    ),
+  );
   styles.set(hash, css);
   return hash;
 }
