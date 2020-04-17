e2b20dfdccc1ceb134306b5acaf80bc81fbd86f3
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-17 10:19:09 +0800

    feat: Render class props

--- src/ssr/fiber.ts
+++ src/ssr/fiber.ts
@@ -1,22 +1,56 @@
 import escape from 'escape-html';
 
+import { CSSReference } from '../csr/css';
 import { Fiber, FiberNode, FiberProps } from '../csr/jsx/vdom';
 
-function renderProps({ children, ...props }: FiberProps): string {
-  const propList = Object.entries(props)
-    .map(([propName, value]) => {
-      if (typeof value === 'boolean') return propName;
-      if (typeof value !== 'number' && typeof value !== 'string') return '';
+function renderClass(classRef: string | CSSReference) {
+  const className =
+    (classRef as CSSReference).className ?? (classRef as string);
 
-      if (value.toString().indexOf('"') !== -1)
-        throw Error(
-          `Invariant violation: prop values should not contain quotes`,
-        );
-      return `${propName}="${value}"`;
-    })
-    .filter(Boolean)
-    .join(' ');
-  return propList.length > 0 ? ' ' + propList : '';
+  if (className.indexOf('"') !== -1) {
+    throw Error(
+      `Invariant violation: unexpected quote found in class name ${className}`,
+    );
+  }
+
+  if (
+    typeof classRef !== 'string' &&
+    classRef.__css_reference !== '__nicessr_css_reference__'
+  ) {
+    throw Error(
+      `Invariant violation: unexpected object recieved as class prop, got ${JSON.stringify(
+        classRef,
+      )}`,
+    );
+  }
+
+  return className;
+}
+
+function renderProps({
+  children,
+  class: className,
+  ...props
+}: FiberProps): string {
+  const propList = Object.entries(props).map(([propName, value]) => {
+    if (typeof value === 'boolean') return propName;
+    if (typeof value !== 'number' && typeof value !== 'string') return '';
+
+    if (value.toString().indexOf('"') !== -1)
+      throw Error(`Invariant violation: prop values should not contain quotes`);
+    return `${propName}="${value}"`;
+  });
+
+  if (className) {
+    if (Array.isArray(className)) {
+      propList.push(`class="${className.map(renderClass).join(' ')}"`);
+    } else {
+      propList.push(`class="${renderClass(className)}"`);
+    }
+  }
+
+  const propsString = propList.filter(Boolean).join(' ');
+  return propsString ? ' ' + propsString : '';
 }
 
 export function renderFiber(fiber: FiberNode | FiberNode[]): string {
