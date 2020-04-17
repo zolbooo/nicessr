70f2a3cc93ccd5b888e5a73bed417cc0d3f5cfb5
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-18 01:26:50 +0800

    chore(eslint): Ignore test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,6 @@ config.rules['@typescript-eslint/camelcase'] = 'off';
 config.rules['import/no-dynamic-require'] = 'off';
 config.rules['no-await-in-loop'] = 'off';
 
+config.ignorePatterns = ['src/**/*.test.ts'];
+
 module.exports = config;
