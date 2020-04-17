99630e67297d413e6c7657b0c01885515625cc39
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-18 01:07:35 +0800

    chore(lint): Disable some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 const config = require('@nicepack/eslint-ts');
 
+config.rules['@typescript-eslint/camelcase'] = 'off';
+config.rules['import/no-dynamic-require'] = 'off';
+
 module.exports = config;
