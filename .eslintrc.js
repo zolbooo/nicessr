4d8cccb8ae6c954cc48c55fa803874f511a02317
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-18 01:13:32 +0800

    chore(lint): Disable no-await-in-loop rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ const config = require('@nicepack/eslint-ts');
 
 config.rules['@typescript-eslint/camelcase'] = 'off';
 config.rules['import/no-dynamic-require'] = 'off';
+config.rules['no-await-in-loop'] = 'off';
 
 module.exports = config;
