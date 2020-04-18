1391000c9f6172104926dbbb9bb2e0761dc23b97
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-18 11:59:52 +0800

    perf: Check for dev environment rather than production one

--- src/csr/errors.ts
+++ src/csr/errors.ts
@@ -1,20 +1,23 @@
 export function handleError(err: Error) {
-  if (process.env.NODE_ENV === 'production') return;
-  require('@pmmmwh/react-refresh-webpack-plugin/src/overlay').handleRuntimeError(
-    err,
-  );
+  if (process.env.NODE_ENV === 'development') {
+    require('@pmmmwh/react-refresh-webpack-plugin/src/overlay').handleRuntimeError(
+      err,
+    );
+  }
 }
 
 export type AnyFunction = (...args: any[]) => void;
 export function injectErrorHandler(fn: AnyFunction): AnyFunction {
-  if (process.env.NODE_ENV === 'production') return fn;
-  return (...args) => {
-    try {
-      fn(...args);
-    } catch (err) {
-      handleError(err);
-    }
-  };
+  if (process.env.NODE_ENV === 'development') {
+    return (...args) => {
+      try {
+        fn(...args);
+      } catch (err) {
+        handleError(err);
+      }
+    };
+  }
+  return fn;
 }
 
 export class SSRError extends Error {
