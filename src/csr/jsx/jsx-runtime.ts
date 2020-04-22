ef68168d5a5964500d65f8348048afbb13599397
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-22 11:47:38 +0800

    fix: Set children to empty array as default

--- src/csr/jsx/jsx-runtime.ts
+++ src/csr/jsx/jsx-runtime.ts
@@ -7,12 +7,14 @@ export const Fragment = 'Fragment';
 export function flattenFragments(root: Fiber): Fiber | Fiber[] {
   if (root.elementName === '#text') return root;
   if (root.elementName === 'Fragment')
-    return (root.props.children as Fiber[])
-      .map((child) => ({ ...child, parent: root.parent }))
-      .map(flattenFragments)
-      .flat(Infinity);
-  root.props.children = (root.props.children as Fiber[])
-    .map(flattenFragments)
-    .flat(Infinity);
+    return (
+      (root.props.children as Fiber[])
+        ?.map((child) => ({ ...child, parent: root.parent }))
+        ?.map(flattenFragments)
+        ?.flat(Infinity) ?? []
+    );
+  root.props.children =
+    (root.props.children as Fiber[])?.map(flattenFragments)?.flat(Infinity) ??
+    [];
   return root;
 }
