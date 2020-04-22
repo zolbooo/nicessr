004d7215adbd80ca3f86360f52eef249056147a4
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-22 11:22:53 +0800

    test: Add tests for fiber renderer

---
+++ src/ssr/fiber.test.ts
@@ -0,0 +1,64 @@
+import { h } from '../csr/jsx/vdom';
+
+import { renderFiber } from './fiber';
+
+describe('Fiber renderer', () => {
+  it('should render empty tags properly', () => {
+    expect(renderFiber(h('div', {}))).toBe('<div></div>');
+  });
+
+  it('should render tags with children properly', () => {
+    expect(renderFiber(h('div', { children: '123' }))).toBe('<div>123</div>');
+    expect(renderFiber(h('div', { children: 123 }))).toBe('<div>123</div>');
+    expect(renderFiber(h('div', { children: [123, 'Hello'] }))).toBe(
+      '<div>123Hello</div>',
+    );
+
+    expect(
+      renderFiber(
+        h('div', { children: [h('p', { children: 'Hello' }), ', world!'] }),
+      ),
+    ).toBe('<div><p>Hello</p>, world!</div>');
+
+    expect(
+      renderFiber(h('div', { children: h('h1', { children: 'x_x' }) })),
+    ).toBe('<div><h1>x_x</h1></div>');
+  });
+
+  it('should render elements with props properly', () => {
+    expect(renderFiber(h('div', { class: 'test' }))).toBe(
+      '<div class="test"></div>',
+    );
+
+    expect(renderFiber(h('div', { class: ['test', 'dev'] }))).toBe(
+      '<div class="test dev"></div>',
+    );
+  });
+
+  it('should render functional components properly', () => {
+    const Title = ({ text }) => h('h1', { children: text });
+    expect(renderFiber(h(Title, { text: 123 }))).toBe('<h1>123</h1>');
+
+    const Container = ({ children }) =>
+      h('div', { class: 'container', children });
+    expect(
+      renderFiber(h(Container, { children: h('h1', { children: 'Hi!' }) })),
+    ).toBe('<div class="container"><h1>Hi!</h1></div>');
+  });
+
+  it('should render boolean props correctly', () => {
+    expect(renderFiber(h('input', { name: 'test', required: true }))).toBe(
+      '<input name="test" required></input>',
+    );
+  });
+
+  it('should render fragments properly', () => {
+    expect(
+      renderFiber(h('Fragment', { children: [h('main', {}), h('aside', {})] })),
+    ).toBe('<main></main><aside></aside>');
+  });
+
+  it('should escape html entities', () => {
+    expect(renderFiber('&<test>')).toBe('&amp;&lt;test&gt;');
+  });
+});
