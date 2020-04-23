6c6a9d9953422059cdbec365e28e81ec9724bf2e
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-23 17:17:00 +0800

    refactor: Remove unused function

--- src/csr/errors.development.ts
+++ src/csr/errors.development.ts
@@ -1,21 +1,5 @@
-export function handleError(err: Error) {
-  if (process.env.NODE_ENV === 'development') {
-    require('@pmmmwh/react-refresh-webpack-plugin/src/overlay').handleRuntimeError(
-      err,
-    );
-  }
-}
+import { handleRuntimeError } from '@pmmmwh/react-refresh-webpack-plugin/src/overlay';
 
-export type AnyFunction = (...args: any[]) => void;
-export function injectErrorHandler(fn: AnyFunction): AnyFunction {
-  if (process.env.NODE_ENV === 'development') {
-    return (...args) => {
-      try {
-        fn(...args);
-      } catch (err) {
-        handleError(err);
-      }
-    };
-  }
-  return fn;
+export function handleError(err: Error) {
+  handleRuntimeError(err);
 }
